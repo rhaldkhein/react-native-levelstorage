0399d9d049f1fdb8a6a50aa051a7c5981e7eae51
kevin <undyingmind@gmail.com>
2020-11-06 02:04:38 +0800

    added test views

--- example/app.js
+++ example/app.js
@@ -7,13 +7,36 @@
 import React from 'react'
 import {
   AppRegistry,
-  View,
-  Text
+  View
 } from 'react-native'
+import SetGetRemove from './components/SetGetRemove'
+import { Buffer } from 'buffer'
+
+const data = [
+  { id: 'string', key: 'key_string', value: 'value_string' },
+  { id: 'number', key: 123, value: 456 },
+  { id: 'buffer', key: Buffer.from([1, 2, 3]), value: Buffer.from([1, 2, 3]) }
+]
 
 function Example() {
-  return <View testID='welcome'>
-    <Text>Example</Text>
+  return <View testID='root'>
+    {
+      data.map(item => {
+        return <View key={item.id}>
+          <SetGetRemove
+            itemValue={item.value}
+            itemKey={item.key}
+            id={item.id}
+            promise={true} />
+          <SetGetRemove
+            itemValue={item.value}
+            itemKey={item.key}
+            id={item.id}
+            promise={false} />
+        </View>
+      })
+    }
+
   </View>
 }
 
---
+++ example/components/SetGetRemove.js
@@ -0,0 +1,61 @@
+// eslint-disable-next-line no-use-before-define
+import React, { useState } from 'react'
+import {
+  View,
+  Button,
+  Text
+} from 'react-native'
+import storage from '../dist'
+
+function SetGetRemove({ id, itemKey, itemValue, promise }) {
+  id = id + (promise ? '_promise' : '_callback')
+  const [value, setValue] = useState('-')
+  const clickSetGet = promise
+    ? async () => {
+        await storage.setItem(itemKey, itemValue)
+        const value = await storage.getItem(itemKey)
+        setValue(value)
+      }
+    : () => {
+        storage.setItem(itemKey, itemValue, err => {
+          if (err) return
+          storage.getItem(itemKey, (err, value) => {
+            if (err) return
+            setValue(value)
+          })
+        })
+      }
+  const clickRemove = promise
+    ? async () => {
+        await storage.removeItem(itemKey)
+        const value = await storage.getItem(itemKey)
+        setValue(value)
+      }
+    : () => {
+        storage.removeItem(itemKey, err => {
+          if (err) return
+          storage.getItem(itemKey, (err, value) => {
+            if (err) return
+            setValue(value)
+          })
+        })
+      }
+
+  return (
+    <View>
+      <Button
+        testID={id + '_setget'}
+        onPress={clickSetGet}
+        title={id + '_setget'} />
+      <Button
+        testID={id + '_remove'}
+        onPress={clickRemove}
+        title={id + '_remove'} />
+      <Text testID={id + '_value'}>
+        {value}
+      </Text>
+    </View>
+  )
+}
+
+export default SetGetRemove
